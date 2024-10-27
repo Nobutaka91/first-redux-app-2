15de9fa79ef43836c0811007228d3d0637015ea4
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2024-10-27 17:58:28 +0900

    リファクタリングとReducerの作成

--- src/index.js
+++ src/index.js
@@ -2,39 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from "redux";
-
-// actions -> increment, decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-// reducer -> actionと前の状態を組み合わせて新しい状態に更新する。
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-// store
-let store = createStore(counterReducer);
-
-// コンソールに新しい状態を出力
-store.subscribe(() => console.log(store.getState()));
-
-// dispatch
-store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
