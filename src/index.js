96ea30b34a6a04c147cd91bbf0f70acc91be3384
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2024-10-27 19:09:47 +0900

    Providerを使用することで、グローバルにStoreを利用できるようにする

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import { createStore } from "redux";
 import allReducers from './reducers';
+import { Provider } from "react-redux"
 
 const store = createStore(
   allReducers, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App store={store}/>
+    </Provider>
   </React.StrictMode>
 );
 
